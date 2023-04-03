73286334562a598f77eb59c472fefb2a6a0dd3bd
Trainer-Learn-to-Code <80373993+Trainer-Learn-to-Code@users.noreply.github.com>
2023-04-03 23:29:57 +0530

    Add files via upload

---
+++ phone-authentication/main.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_signin_integration/phone-authentication/phone.dart';
+import 'package:google_signin_integration/phone-authentication/verify.dart';
+
+import 'home.dart';
+
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    initialRoute: 'phone',
+    debugShowCheckedModeBanner: false,
+    routes: {
+      'phone': (context) => MyPhone(),
+      'verify': (context) => MyVerify(),
+      'home':((context) => Home())
+    },
+  ));
+}
